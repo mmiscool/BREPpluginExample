2c415ec12d01e020ee7df2cecefb37bf81f05b2e
Mike <mike@smbisoft.com>
2025-09-24 17:30:53 -0400

    Testing making a feature

---
+++ exampleFeature.js
@@ -0,0 +1,61 @@
+
+
+const inputParamsSchema = {
+    featureID: {
+        type: 'string',
+        default_value: null,
+        hint: 'Unique identifier for the feature'
+    },
+    radius: {
+        type: 'number',
+        default_value: 5,
+        hint: 'Radius of the sphere'
+    },
+    resolution: {
+        type: 'number',
+        default_value: 32,
+        hint: 'Base segment count (longitude). Latitude segments are derived from this.'
+    },
+    transform: {
+        type: 'transform',
+        default_value: { position: [0, 0, 0], rotationEuler: [0, 0, 0], scale: [1, 1, 1] },
+        hint: 'Position, rotation, and scale'
+    },
+    boolean: {
+        type: 'boolean_operation',
+        default_value: { targets: [], operation: 'NONE' },
+        hint: 'Optional boolean operation with selected solids'
+    }
+};
+
+export class PrimitiveSphereFeaturePlugin {
+    static featureShortName = "S.p";
+    static featureName = "Primitive Sphere";
+    static inputParamsSchema = inputParamsSchema;
+
+    constructor() {
+        this.inputParams = {};
+
+        this.persistentData = {};
+    }
+
+    async run(partHistory) {
+        const { radius, resolution, featureID } = this.inputParams;
+
+        const sphere = await new BREP.Sphere({
+            r: radius,
+            resolution,
+            name: featureID,
+        });
+        try {
+            if (this.inputParams.transform) {
+                sphere.bakeTRS(this.inputParams.transform);
+            }
+        } catch (_) { }
+        sphere.visualize();
+
+        alert("Hello from plugin feature");
+
+        return await BREP.applyBooleanOperation(partHistory || {}, sphere, this.inputParams.boolean, featureID);
+    }
+}
--- plugin.js
+++ plugin.js
@@ -1,3 +1,5 @@
+import { PrimitiveSphereFeaturePlugin } from './exampleFeature.js';
+
 let BREP;
 
 export default (app) => {
@@ -13,80 +15,6 @@ export default (app) => {
         div.textContent = 'This was added by a plugin.';
         return div;
     });
-     app.registerFeature(PrimitiveSphereFeaturePlugin); // optional
+    app.registerFeature(PrimitiveSphereFeaturePlugin); // optional
 };
-
-
-
-
-
-const inputParamsSchema = {
-    featureID: {
-        type: 'string',
-        default_value: null,
-        hint: 'Unique identifier for the feature'
-    },
-    radius: {
-        type: 'number',
-        default_value: 5,
-        hint: 'Radius of the sphere'
-    },
-    resolution: {
-        type: 'number',
-        default_value: 32,
-        hint: 'Base segment count (longitude). Latitude segments are derived from this.'
-    },
-    transform: {
-        type: 'transform',
-        default_value: { position: [0, 0, 0], rotationEuler: [0, 0, 0], scale: [1, 1, 1] },
-        hint: 'Position, rotation, and scale'
-    },
-    boolean: {
-        type: 'boolean_operation',
-        default_value: { targets: [], operation: 'NONE' },
-        hint: 'Optional boolean operation with selected solids'
-    }
-};
-
-export class PrimitiveSphereFeaturePlugin {
-    static featureShortName = "S.p";
-    static featureName = "Primitive Sphere";
-    static inputParamsSchema = inputParamsSchema;
-
-    constructor() {
-        this.inputParams ={};
-
-        this.persistentData = {};
-    }
-
-    async run(partHistory) {
-        const { radius, resolution, featureID } = this.inputParams;
-
-        const sphere = await new BREP.Sphere({
-            r: radius,
-            resolution,
-            name: featureID,
-        });
-        try {
-            if (this.inputParams.transform) {
-                sphere.bakeTRS(this.inputParams.transform);
-            }
-        } catch (_) { }
-        sphere.visualize();
-
-        return await BREP.applyBooleanOperation(partHistory || {}, sphere, this.inputParams.boolean, featureID);
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
 
